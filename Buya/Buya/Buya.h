7c02b22e702036ce94b917eb3f69d4c723e558d5
Puasonych <basargin.erik@gmail.com>
2019-03-02 15:29:01 +0500

    First init

---
+++ Buya/Buya/Buya.h
@@ -0,0 +1,19 @@
+//
+//  Buya.h
+//  Buya
+//
+//  Created by Eric Basargin on 02/03/2019.
+//  Copyright © 2019 Three-man army. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Buya.
+FOUNDATION_EXPORT double BuyaVersionNumber;
+
+//! Project version string for Buya.
+FOUNDATION_EXPORT const unsigned char BuyaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Buya/PublicHeader.h>
+
+
