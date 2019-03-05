b523e94f19046bae15e0829e1810d0dc8790fc8c
Puasonych <basargin.erik@gmail.com>
2019-03-05 19:50:08 +0500

    Added map to custom struct and upgrade to 0.0.3 version

---
+++ Buya/Extensions/Single+Data.swift
@@ -0,0 +1,22 @@
+//
+//  Single+Data.swift
+//  Buya
+//
+//  Created by Eric Basargin on 05/03/2019.
+//  Copyright © 2019 Three-man army. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+public extension Single where Element == Data {
+    public func map<T: Decodable>(_ type: T.Type, using decoder: JSONDecoder = JSONDecoder()) -> Single<T> {
+        return self.asObservable().flatMap({ (data) -> Observable<T> in
+            do {
+                return Observable.just(try decoder.decode(type, from: data))
+            } catch {
+                return Observable.error(error)
+            }
+        }).asSingle()
+    }
+}
